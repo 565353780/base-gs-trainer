5640141694bc8ef97dfb4f14dc90cebf1ba2e575
chLi <565353780@qq.com>
2026-02-13 14:01:17 +0800

    add more necessary package

--- dev_setup.sh
+++ dev_setup.sh
@@ -1,9 +1,19 @@
 cd ..
 git clone github@github.com:565353780/base-trainer.git
 git clone github@github.com:565353780/camera-control.git
+git clone --depth 1 https://github.com/camenduru/simple-knn.git
+git clone --depth 1 https://github.com/rahul-goel/fused-ssim.git
 
 cd base-trainer
 ./dev_setup.sh
 
 cd ../camera-control
 ./dev_setup.sh
+
+cd ../simple-knn
+python setup.py install
+
+cd ../fused-ssim
+python setup.py install
+
+pip install plyfile
--- setup.sh
+++ setup.sh
@@ -1,9 +1,19 @@
 cd ..
 git clone https://github.com/565353780/base-trainer.git
 git clone https://github.com/565353780/camera-control.git
+git clone --depth 1 https://github.com/camenduru/simple-knn.git
+git clone --depth 1 https://github.com/rahul-goel/fused-ssim.git
 
 cd base-trainer
 ./setup.sh
 
 cd ../camera-control
 ./setup.sh
+
+cd ../simple-knn
+python setup.py install
+
+cd ../fused-ssim
+python setup.py install
+
+pip install plyfile
