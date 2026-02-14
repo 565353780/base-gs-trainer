8121a198cf86a02f148884028aacaa1ef9c4d213
chLi <565353780@qq.com>
2026-02-14 11:01:46 +0800

    add cx cy support for gs transform matrix

--- base_gs_trainer/Data/gs_camera.py
+++ base_gs_trainer/Data/gs_camera.py
@@ -2,7 +2,11 @@ import torch
 
 from camera_control.Module.camera import Camera
 
-from base_gs_trainer.Method.graphics_utils import getProjectionMatrix, focal2fov
+from base_gs_trainer.Method.graphics_utils import (
+    focal2fov,
+    getProjectionMatrix,
+    getGSProjectionMatrix_from_intrinsics,
+)
 
 
 class GSCamera:
@@ -27,7 +31,14 @@ class GSCamera:
         self.image_name = cam.image_id
 
         self.world_view_transform = cam.world2cameraColmap.transpose(0, 1).cuda()
-        self.projection_matrix = getProjectionMatrix(self.FoVx, self.FoVy).transpose(0, 1).cuda()
+
+        # self.projection_matrix = getProjectionMatrix(self.FoVx, self.FoVy).transpose(0, 1).cuda()
+        # 2. 抛弃旧的基于 FOV 的 getProjectionMatrix
+        self.projection_matrix = getGSProjectionMatrix_from_intrinsics(
+            fx=cam.fx, fy=cam.fy, cx=cam.cx, cy=cam.cy,
+            width=cam.width, height=cam.height,
+        ).transpose(0, 1).cuda()
+
         self.full_proj_transform = (self.world_view_transform.unsqueeze(0).bmm(self.projection_matrix.unsqueeze(0))).squeeze(0)
 
         self.camera_center = cam.camera2worldColmap[:3, 3].cuda()
--- base_gs_trainer/Method/graphics_utils.py
+++ base_gs_trainer/Method/graphics_utils.py
@@ -2,6 +2,12 @@ import math
 import torch
 
 
+def fov2focal(fov, pixels):
+    return pixels / (2 * math.tan(fov / 2))
+
+def focal2fov(focal, pixels):
+    return 2*math.atan(pixels/(2*focal))
+
 def getProjectionMatrix(fovX, fovY, znear: float=0.01, zfar: float=100.0):
     tanHalfFovY = math.tan((fovY / 2))
     tanHalfFovX = math.tan((fovX / 2))
@@ -24,8 +30,22 @@ def getProjectionMatrix(fovX, fovY, znear: float=0.01, zfar: float=100.0):
     P[2, 3] = -(zfar * znear) / (zfar - znear)
     return P
 
-def fov2focal(fov, pixels):
-    return pixels / (2 * math.tan(fov / 2))
+def getGSProjectionMatrix_from_intrinsics(
+    fx: float, fy: float, cx: float, cy: float, 
+    width: int, height: int,
+    znear: float = 0.01, zfar: float = 100.0
+) -> torch.Tensor:
+    P = torch.zeros(4, 4)
+    z_sign = 1.0
 
-def focal2fov(focal, pixels):
-    return 2*math.atan(pixels/(2*focal))
+    P[0, 0] = 2.0 * fx / width
+    P[1, 1] = 2.0 * fy / height
+
+    # 找回丢失的主点偏移
+    P[0, 2] = (2.0 * cx / width) - 1.0
+    P[1, 2] = 1.0 - (2.0 * cy / height)  # 适配左下角cy定义 + 3DGS朝向
+
+    P[3, 2] = z_sign
+    P[2, 2] = z_sign * zfar / (zfar - znear)
+    P[2, 3] = -(zfar * znear) / (zfar - znear)
+    return P
