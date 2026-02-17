92e8497cc7806ca1c22e10a6771b2737e90a7375
chLi <565353780@qq.com>
2026-02-18 00:31:38 +0800

    add get cameras spatial extent algo

--- base_gs_trainer/Method/camera_utils.py
+++ base_gs_trainer/Method/camera_utils.py
@@ -20,3 +20,16 @@ def cameras_extent_from_list(cam_list: List[Camera]):
     dist = np.linalg.norm(centers - center, axis=0, keepdims=True)
     diagonal = float(np.max(dist))
     return diagonal * 1.1
+
+def get_cameras_spatial_extent(cameras:List[Camera]):
+    cam_centers = torch.cat([camera.pos.view(1, 3) for camera in cameras], dim=0)
+
+    avg_cam_center = torch.mean(cam_centers, dim=0, keepdim=True)
+    dist = torch.norm(cam_centers - avg_cam_center, dim=1, keepdim=True)
+
+    half_diagonal = torch.max(dist)
+    radius = half_diagonal * 1.1
+
+    translate = -avg_cam_center
+
+    return {"translate": translate, "radius": radius, "avg_cam_center": avg_cam_center}
