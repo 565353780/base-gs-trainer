8d4ff174de0690d633c0b6fe2e6fa13a323c0094
chLi <565353780@qq.com>
2026-04-09 13:48:05 +0800

    update image call

--- base_gs_trainer/Data/gs_camera.py
+++ base_gs_trainer/Data/gs_camera.py
@@ -36,7 +36,10 @@ class GSCamera:
 
         # 图像：(H, W, 3) -> (3, H, W)，并应用 mask（若有）
         if cam.height == cam.image.shape[0] and cam.width == cam.image.shape[1]:
-            self.original_image = cam.toMaskedImage().permute(2, 0, 1).clamp(0.0, 1.0).to(device)
+            self.original_image = cam.toImageVis(
+                background_color=[255, 255, 255],
+                use_mask=True,
+            ).permute(2, 0, 1).clamp(0.0, 1.0).to(device)
         #self.original_image = cam.image.permute(2, 0, 1).clamp(0.0, 1.0).to(device)
         self.image_name = cam.image_id
 
